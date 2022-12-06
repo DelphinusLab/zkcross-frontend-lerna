546820e7611f0aa46c3b52c6002b7800966c08fd
Richard Hao <richard@rhao.io>
2022-12-06 18:13:41 +1100

    init

---
+++ setup-frontend.sh
@@ -0,0 +1,9 @@
+[ -d "./packages" ] || mkdir packages
+
+cd packages
+repo init -u git@github.com:DelphinusLab/delphinus.git -b split -m delphinus-frontend.xml
+repo sync
+cd -
+
+npm install
+npx lerna bootstrap --hoist --nohoist=circomlib
